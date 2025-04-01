cedb357f66381ab523dc83ec70e5b25d4b81065d
Prabhat Singh <ps2656728@gmail.com>
2025-04-02 00:30:20 +0530

    start from initial

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,9 @@
-import './App.css';
-import ApplyForm from './pages/ApplyForm';
-import Navbar from './components/Navbar'; // Corrected import path
-import InternshipDetails from './pages/InternshipDetails';
-import InternshipTraining from './pages/InternshipTraining';
-import Career from './pages/Career';
-import Service from './pages/Service'
-import { BrowserRouter as Router } from 'react-router-dom'; // Added `Router`
-import { Routes, Route } from 'react-router-dom'; // Added `Route`
-import Insight from './pages/Insight';
-import ScalableAWSPage from './pages/Insight2';
-import AboutUs from './pages/AboutUs';
-import ContactUs from './pages/ContactUs';
-import Blogs from './pages/Blogs';
-import Footer from './components/Footer';
+
 
 function App() {
   return (
     <>
-    
-
-       <Navbar/>
-      <Routes>
-        <Route path="/insights" element={<Insight/>} />
-        <Route path="/details" element={<InternshipDetails />} />
-        <Route path="/training" element={<InternshipTraining />} />
-        <Route path="/career" element={<Career />} />
-        <Route path="/service" element={<Service />} />
-        <Route path="/blogs" element ={<Blogs/>}/>
-        <Route path ="/about" element={<AboutUs />} />
-        <Route path="contact" element ={< ContactUs/>}/>
-      </Routes>
-       <Footer />
+    {/* START YOUR CODE FROM HERE  */}
     </>
   );
 }
--- src/components/InternshipTraining.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import InternshipCard from "./InternshipCard";
-
-const InternshipTraining = ({ internship }) => {
-  return <InternshipCard internship={internship} />;
-};
-
-export default InternshipTraining;
